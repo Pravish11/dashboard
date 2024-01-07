44c73e0b37dc10d7b721d23f8f6ebb802bd253b6
Pravish11 <pravish.digumber@umail.uom.ac.mu>
2024-01-07 15:55:33 +0400

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # dashboard
 
+Dashboard website: https://dashboardweb.web.app/
+
+The dashboard was built using flutter and deployed on firebase.
+The link is displayed above.
+
 https://youtu.be/_uOgXpEHNbc?si=xVm5JQt6ldRwYn-D
 
 ![Alt text](image.png)
--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,16 @@ import 'package:dashboard/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,13 +25,25 @@ class MyApp extends StatelessWidget {
             .apply(bodyColor: Colors.white),
         canvasColor: secondaryColor,
       ),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => MenuAppController(),
-          )
-        ],
-        child: MainScreen(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MultiProvider(
+              providers: [
+                ChangeNotifierProvider(
+                  create: (context) => MenuAppController(),
+                )
+              ],
+              child: MainScreen(),
+            );
+          }
+
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
